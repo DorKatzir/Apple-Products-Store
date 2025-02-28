4d6a758984f64e8eb6c9cb3205de5ba8a13288f3
Dor Katzir <katzirdor@gmail.com>
2025-02-28 16:17:52 +0200

    implementing Zooming Functionality

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function Controls() {
+function Controls({ toggleZoom, frameZoom }) {
   return (
-    <div>Controls</div>
+    <div className='absolute top-3 right-3 space-x-2'>
+        <button onClick={toggleZoom} className='text-2xl text-pink-400 cursor-pointer'>
+            <i className={ frameZoom ? 'bx bxs-zoom-out' : 'bx bxs-zoom-in'}></i>
+        </button>
+        <button className='text-2xl text-pink-400'>
+            <i className='bx bx-x-circle'></i>
+        </button>
+    </div>
   )
 }
 
