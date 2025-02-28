bbaeb225a0bdc80f0debd3b2fa9ee028f7843745
Dor Katzir <katzirdor@gmail.com>
2025-02-28 20:05:07 +0200

    Implementing  Navbar Items Data

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,23 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
-  const toggleZoom = () => setFrameZoom(!frameZoom)
+  const [activePage, setActivePage] = useState(0)
+
+  function toggleZoom(){
+    setFrameZoom(!frameZoom)
+  }
+
+  function handleNavClick (pageIndex) {
+    setActivePage(pageIndex)
+  }
 
   return (
     <div className='w-full h-screen grid place-items-center'>
-      <div className={` ${frameZoom && 'min-w-[97vw] min-h-[97vh]'} 
-                        w-[70vw] 
-                        h-[85vh] 
-                        min-w-[70vw] 
-                        min-h-[85vh] 
-                        max-w-[90vw] 
-                        max-h-[90vh]
-                        border
-                        border-gray-300
-                        rounded-2xl 
-                        resize
-                        overflow-auto
-                        relative
-                        transition-all
-                        duration-100 `}>
-        <Navbar />                  
+      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh borde border-gray-30 rounded-2xl resiz overflow-aut relativ transition-al duration-100`}>
+        
+        <Navbar handleNavClick={handleNavClick} activePage={activePage} />                  
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />  
+
       </div>
     </div>
   )
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Controls({ toggleZoom, frameZoom }) {
   return (
     <div className='absolute top-3 right-3 space-x-2'>
-        <button onClick={toggleZoom} className='text-2xl text-pink-400 cursor-pointer bx-flashing-hover'>
+        <button onClick={toggleZoom} className='text-2xl text-pink-400 cursor-pointer'>
             <i className={ frameZoom ? 'bx bxs-zoom-out' : 'bx bxs-zoom-in'}></i>
         </button>
         <button className='text-2xl text-pink-400'>
