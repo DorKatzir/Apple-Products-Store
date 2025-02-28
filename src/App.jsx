59667e7e3b2c647d249f731e895f26740478afa8
Dor Katzir <katzirdor@gmail.com>
2025-02-28 21:34:26 +0200

    Building the Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import Controls from './components/Controls'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+
 
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
@@ -17,10 +19,13 @@ const App = () => {
 
   return (
     <div className='w-full h-screen grid place-items-center'>
-      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
+      <div className={`flex ${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
         
         <Navbar handleNavClick={handleNavClick} activePage={activePage} />                  
-        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />  
+        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
+        <div className='flex-grow'>
+          <Home />
+        </div>  
 
       </div>
     </div>
