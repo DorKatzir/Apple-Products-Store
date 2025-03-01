77b77103718b5f94c65643fa3dbf4077778f016f
Dor Katzir <katzirdor@gmail.com>
2025-03-01 16:57:48 +0200

    Creating the Macbooks Page and implementing the Models Buttons

--- src/components/Macbook.jsx
+++ src/components/Macbook.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { macbookModels } from '../data/index'
 
 const Macbook = () => {
+
+    const [selectedChip, setSelectedChip] = useState(0)
+
+    function handleSelectedChip(index) {
+        setSelectedChip(index)
+    }
+
   return (
     <div className='flex flex-col items-center justify-around bg-white p-8'>
         
@@ -9,10 +16,18 @@ const Macbook = () => {
             Choose your new MacBook Air.
         </h1>
         
-        <button className='2xl:w-36 md:w-26 w-28 2xl:h-18 md:h-12 h-12 flex flex-col justify-evenly items-center border rounded-l-xl'>
-            <span className='2xl:text-lg xl:text-sm text-xs font-bold text-gray-800'>With M2 chip</span>
-        </button>
-         
+        <div className='flex'>
+            {macbookModels.map((macbook, index) => (
+                 <button onClick={()=>handleSelectedChip(index)} key={ index } className={`2xl:w-36 md:w-26 w-28 2xl:h-18 md:h-12 h-12 flex flex-col justify-evenly items-center border ${index === 0 ? 'rounded-l-xl' : 'rounded-r-xl'} ${index === selectedChip ? 'border-2 border-blue-400' : 'border-gray-800'}`}
+                 >
+                    <span className='2xl:text-lg xl:text-sm text-xs font-bold text-gray-800'>with { macbook.chip } chip</span>
+                    <span className='2xl:text-base xl:text-sm text-xs text-gray-600'>{ macbook.price }</span>
+                </button>
+            ))}          
+        </div>
+
+        
+
     </div>
   )
 }
