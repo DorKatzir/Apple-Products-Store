e5e57b795f40a075515d14bb1bfd7da4b96be556
Dor Katzir <katzirdor@gmail.com>
2025-03-05 07:46:54 +0200

    Adding Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react'
+import React, {useState, useEffect} from 'react'
 import Controls from './components/Controls'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -14,9 +13,22 @@ import PageTransition from './components/PageTransition'
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
   const [activePage, setActivePage] = useState(0)
+  const [isLgScreen, setIsLgScreen] = useState(window.innerWidth > 1024)
+
+  useEffect(() => {
+    function handleResize() {
+      setIsLgScreen(window.innerWidth >= 1024)
+      setFrameZoom(true)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])  
+
 
   function toggleZoom(){
-    setFrameZoom(!frameZoom)
+    if(isLgScreen) {
+      setFrameZoom(!frameZoom)
+    }
   }
 
   function handleNavClick (pageIndex) {
@@ -32,7 +44,7 @@ const App = () => {
       <div className={`flex ${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
         
         <Navbar handleNavClick={handleNavClick} activePage={activePage} />                  
-        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} resetPage={resetPage} activePage={activePage} />
+        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} resetPage={resetPage} activePage={activePage} isLgScreen={isLgScreen} />
         <div className='flex-grow'>
           <PageTransition activePage={activePage}>
             <Home onNavigate={handleNavClick} />
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Controls({ toggleZoom, frameZoom, resetPage, activePage }) {
   return (
     <div className='absolute top-3 right-3 space-x-2 z-10'>
-        <button onClick={toggleZoom} className='text-2xl text-pink-400 cursor-pointer'>
+      
+        <button onClick={toggleZoom} className='text-2xl text-pink-400 cursor-pointer hidden lg:inline-block'>
             <i className={ frameZoom ? 'bx bxs-zoom-out' : 'bx bxs-zoom-in'}></i>
         </button>
         <button onClick={ resetPage } className={`text-2xl ${activePage === 0 ? 'text-red-200 cursor-not-allowed' : 'text-pink-400 cursor-pointer'}`}>
